a7991b719e08f2dcd1913df4a152e09b13f25b4f
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-10 19:09:07 -0300

    Requisitos 1 e 2 concluídos

--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -1,27 +1,30 @@
-// const { getMagicCard } = require('../magic');
+const { getMagicCard } = require('../magic');
 // require('../../startest/mockSetup');
 
 jest.setTimeout(10000);
 
 describe('1 - Testes da função getMagicCard', () => {
   it('1.1 Verifique se getMagicCard é uma função.', () => {
-    // implemente seus testes aqui
+    expect.assertions(1);
+    expect(typeof getMagicCard).toBe('function');
   });
 
   it('1.2 Verifique se a função fetch foi chamada.', async () => {
-    // await getMagicCard('130550');
-    // implemente seus testes aqui
+    await getMagicCard('130550');
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 
   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
-    // await getMagicCard('130550');
-    // implemente seus testes aqui
+    const endpoint = '130550';
+    await getMagicCard('130550');
+    expect(fetch).toHaveBeenCalledWith(`https://api.magicthegathering.io/v1/cards/${endpoint}`);
   });
 });
 
 describe('2 - Testando o retorno da função getMagicCard', () => {
   it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
-    // const response = await getMagicCard('130550');
-    // implemente seus testes aqui
+    const response = await getMagicCard('130550');
+    const { name } = response;
+    expect(name).toMatch(/Ancestor's Chosen/);
   });
 });
